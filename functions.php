7c8fca480e6089a7c00ec00a1466169c8e53672e
Jhonkar Sufia <windyforest13@gmail.com>
2021-09-22 19:18:15 -0400

    integrating bootstrap and montserrat font on functions.php

--- functions.php
+++ functions.php
@@ -6,3 +6,13 @@ function init_template() {
 }
 
 add_action('after_setup_theme', 'init_template');
+
+
+function assets() {
+    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1', 'all');
+    wp_register_style('montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap', '', '1.0', 'all');
+    
+    wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap', 'montserrat'), '1.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'assets');
