fabfe05684c1e738f3f21eb2b98e8ea4e2b701ed
Jhonkar Sufia <windyforest13@gmail.com>
2021-09-23 15:57:14 -0400

    adding js scripts, styles, a logo, and a menu

--- functions.php
+++ functions.php
@@ -3,16 +3,31 @@
 function init_template() {
     add_theme_support('post-thumnails');
     add_theme_support('title-tag');
+
+    //menu
+    register_nav_menus( 
+        array(
+            'top menu' => 'menu principal'
+        )
+    );
 }
 
 add_action('after_setup_theme', 'init_template');
 
 
 function assets() {
-    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1', 'all');
-    wp_register_style('montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap', '', '1.0', 'all');
+    //Style libraries
+    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '5.1.1', 'all');
+    wp_register_style('montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap', '', '1.1', 'all');
     
     wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap', 'montserrat'), '1.0', 'all');
+
+    //script libraries
+    wp_register_script('popperjs', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js', '', '2.10.1', true);
+    wp_enqueue_script('bootstrapjs', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js', array('popperjs'), '5.1.1', true);
+
+    //My own scripts
+    wp_enqueue_script('custom', get_template_directory_uri().'/assets/js/custom.js', '', '0.1', true);
 }
 
 add_action('wp_enqueue_scripts', 'assets');
--- header.php
+++ header.php
@@ -6,10 +6,25 @@
         <meta name="robots" content="index, follow" />
         <title>Platzigifts</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="style.css" />
         <?php wp_head() ?>
     </head>
     <body>
         <header>
-
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-4">
+                        <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" alt="logo"/>
+                    </div>
+                    <div class="col-8">
+                        <?php wp_nav_menu(
+                            array(
+                                'theme_location' => 'top_menu',
+                                'menu_class' => 'menu_principal',
+                                'container_class' => 'container-menu'
+                            )
+                        ); 
+                        ?>
+                    </div>
+                </div>
+            </div>
         </header>
