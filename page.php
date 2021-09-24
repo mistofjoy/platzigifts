24fecbb23b06ed4fc47717d08d2d751249f11c8a
Jhonkar Sufia <windyforest13@gmail.com>
2021-09-24 14:05:40 -0400

    fixes to page.php and adding single.php and more images

--- page.php
+++ page.php
@@ -4,10 +4,12 @@
             if(have_posts()) {
                 while(have_posts()) {
                     the_post();
+                    ?>
+                    <h1 class="my-3"><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                    <?php
                 }
             }
-        ?>
-        <h1 class="my-3"><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+        ?>  
     </main>
 <?php get_footer(); ?>
