79cbdeced631de63817240af4ad401f192bb994e
Jhonkar Sufia <windyforest13@gmail.com>
2021-09-28 18:20:22 -0400

    adding a 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<main class="container">
+    <div class="pagina404 my-5">
+        <h1>404 - pagina no encontrada</h1>
+        <h2>Click <a href="<?php echo home_url(); ?>">aqui</a> para volver a la pagina principal</h2>
+    </div>
+</main>
+
+<?php get_footer(); ?>
